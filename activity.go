bbb0d29b2aaf973d6e88235df96f6338d53b8dc2
eswarabhi <eswar.abhi1210@gmail.com>
2018-12-03 21:34:05 +0530

    Update activity.go

--- activity.go
+++ activity.go
@@ -20,7 +20,7 @@ var activityLog = logger.GetLogger("activity-colored_logger")
 
 func init() {
 	activityLog.SetLogLevel(logger.InfoLevel)
-	dbg := fmt.Sprintf("%+v",logger)
+	dbg := fmt.Sprintf("%+v",activityLog)
 	logger.Info(dbg)
 }
 
